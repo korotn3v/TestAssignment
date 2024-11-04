82a6c4b2504c0201e72b172a48dea927c4fc77f5
Anton <korotnev09@gmail.com>
2024-11-04 19:37:09 +0200

    Fixing Bugs

--- src/main/java/org/jetbrains/car/Car.java
+++ src/main/java/org/jetbrains/car/Car.java
@@ -27,7 +27,7 @@ public abstract class Car {
     }
 
     public void driveTo(double destination){
-        double distance = destination-this.location;
+        double distance = Math.abs(destination-this.location);
         this.energy.reduceEnergy(distance*energyUsageRate);
         this.location = destination;
     }
@@ -57,7 +57,7 @@ public abstract class Car {
         }
 
         public double getEnergy() {
-            return energy++;
+            return energy;
         }
 
         public void recharge(){
